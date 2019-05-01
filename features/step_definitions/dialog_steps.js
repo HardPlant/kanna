e41365312b2b15e91bcf9a95fe9650ede13779a7
HardPlant <abc7988se@naver.com>
2019-05-01 20:59:55 +0900

    대화상자 기능

---
+++ features/step_definitions/dialog_steps.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+const fs = require('fs');
+
+Given('스테이지 ID {string}가 주어진다', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+When('대화 상자 표시를 시작하면', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('해당 ID 하위 대화 모델이 로드된다', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+When('진행하면', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('다음 대화 모델이 로드된다', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
